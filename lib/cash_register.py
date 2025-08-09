2d0f9d4a9bed3b6cd7dc9af6c7856548f3a42c5d
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-08 22:54:36 -0600

    create a CashRegister class and set properties and setters

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount = 0, total = 0, items = None, previous_transactions = None):
+    self.discount = discount
+    self.total = total
+    self.items = items if items is not None else []
+    self.previous_transactions = previous_transactions if previous_transactions is not None else []
+
+# discount property
+@property
+def discount(self):
+  return self._discount
+
+@discount.setter
+def discount(self, value):
+  if isinstance(discount, int) and 0 <= discount <= 100:
+    self._discount = discount
+  else:
+      print("Not valid discount")
