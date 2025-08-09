f9eb1bc2c2e313701ac9f142768ddc7491723029
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-08 23:13:30 -0600

    create add_item method that accepts item, price, and quantiy attributes, passes relevant tests

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -7,14 +7,29 @@ class CashRegister:
     self.items = items if items is not None else []
     self.previous_transactions = previous_transactions if previous_transactions is not None else []
 
-# discount property
-@property
-def discount(self):
-  return self._discount
+  # discount property
+  @property
+  def discount(self):
+    return self._discount
 
-@discount.setter
-def discount(self, value):
-  if isinstance(discount, int) and 0 <= discount <= 100:
-    self._discount = discount
-  else:
-      print("Not valid discount")
+  @discount.setter
+  def discount(self, value):
+    if isinstance(value, int) and 0 <= value <= 100:
+      self._discount = value
+    else:
+        print("Not valid discount")
+
+  def add_item(self, item, price, quantity = 1):
+    # update the running total
+    self.total += price * quantity
+    
+    #add items to the array
+    self.items.extend([item] * quantity)
+
+    # record the transaction as an object to append into previous_transactions array
+    self.previous_transactions.append({
+        "item": item,
+        "price": price,
+        "quantity": quantity,
+        "subtotal": price * quantity
+    })
